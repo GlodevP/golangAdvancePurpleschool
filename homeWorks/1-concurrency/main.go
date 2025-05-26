1f573b221bf95b96bc9d7105443c4418235ccde1
GlodevP <glodevp@gmail.com>
2025-05-26 18:52:16 +0300

    1-concurrency hw

---
+++ homeWorks/1-concurrency/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	sl := make([]int, 10)
+	chIn := make(chan int)
+	chOut := make(chan int)
+	go random10SliceInChanel(chIn)
+	go sqrt10ChInChOut(chIn, chOut)
+	for i := range sl {
+		sl[i] = <-chOut
+	}
+	close(chOut)
+	for i := range sl {
+		fmt.Printf("%d ", sl[i])
+	}
+}
+
+func sqrt10ChInChOut(chIn chan int, chOut chan int) {
+	for range 10 {
+		j := <-chIn
+		chOut <- j * j
+	}
+	close(chIn)
+}
+
+func random10SliceInChanel(ch chan int) {
+	sl := make([]int, 10)
+	for i := range 10 {
+		sl[i] = rand.Intn(101)
+	}
+	for i := range sl {
+		ch <- sl[i]
+	}
+}
