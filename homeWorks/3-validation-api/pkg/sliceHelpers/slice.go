95d14a720482f78f15c213d5dbf3e750a6dfbdec
GlodevP <glodevp@gmail.com>
2025-06-16 17:41:56 +0300

    remove befo verificate

--- homeWorks/3-validation-api/pkg/sliceHelpers/slice.go
+++ homeWorks/3-validation-api/pkg/sliceHelpers/slice.go
@@ -1,10 +1,18 @@
 package slicehelpers
 
-func ContainsStringInStringSlicce(s []string, e string) bool {
+func ContainsInStringSlice(s []string, e string) bool {
 	for _, a := range s {
 		if a == e {
 			return true
 		}
 	}
 	return false
+}
+func RemoveInStringSlice(s []string, e string) []string {
+	for i, a := range s {
+		if a == e {
+			return append(s[:i], s[i+1:]...)
+		}
+	}
+	return s
 }
