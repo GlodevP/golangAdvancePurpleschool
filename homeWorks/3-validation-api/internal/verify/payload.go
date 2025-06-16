5a06bd22ffe8ffeb03c2bfb8aaea1d9693359f49
GlodevP <glodevp@gmail.com>
2025-06-16 18:30:44 +0300

    add port

--- homeWorks/3-validation-api/internal/verify/payload.go
+++ homeWorks/3-validation-api/internal/verify/payload.go
@@ -1,12 +1,12 @@
 package verify
 
 type SendRequest struct {
-	Email string `json: "Email" validate: "required,email"`
+	Email string `json:"Email" validate:"required,email"`
 }
 
 type VerifyResponse struct {
-	Success bool `json: "Success"`
+	Success bool `json:"Success"`
 }
 type SendResponse struct {
-	Success bool `json: "Success"`
+	Success bool `json:"Success"`
 }
