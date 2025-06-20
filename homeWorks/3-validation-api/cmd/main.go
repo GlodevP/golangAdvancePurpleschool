29d254b2437a634b0ae15eea7239d5a76c4d192d
GlodevP <glodevp@gmail.com>
2025-06-20 11:14:40 +0300

    fix

--- homeWorks/3-validation-api/cmd/main.go
+++ homeWorks/3-validation-api/cmd/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"3-validation-api/configs"
 	"3-validation-api/internal/verify"
+	"log"
 	"net/http"
 )
 
 func main() {
 	cfg := configs.NewConfigs()
 	router := http.NewServeMux()
-	verify.NewVerifyHandler(cfg, router)
+	err := verify.NewVerifyHandler(cfg, router)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	http.ListenAndServe(cfg.Url, router)
 }
