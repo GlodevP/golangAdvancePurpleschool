ef6849d3239cce6e18c77453b22692334a7447a8
GlodevP <glodevp@gmail.com>
2025-06-23 15:32:57 +0300

    gorm init

--- exercises/6.5 Модуль авторизации/cmd/main.go
+++ exercises/6.5 Модуль авторизации/cmd/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"temp/configs"
 	"temp/internal/auth"
+	"temp/pkg/db"
 )
 
 func main() {
 	cfg := configs.LoadConfig()
+	_,err := db.NewDB(cfg)
+	if err != nil{
+		log.Fatalln("Error, not connect to db,",err)
+	}
 	router := http.NewServeMux()
 	auth.NewAuthHandler(router, &auth.AuthHandlerDeps{
 		Config: &cfg,
--- exercises/6.5 Модуль авторизации/pkg/db/db.go
+++ exercises/6.5 Модуль авторизации/pkg/db/db.go
@@ -1,1 +1,20 @@
 package db
+
+import (
+	"temp/configs"
+
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+)
+
+type DB struct{
+	*gorm.DB
+}
+
+func NewDB(conf configs.Config) (*DB,error) {
+	db,err := gorm.Open(postgres.Open(conf.DBConfig.DSN))
+	if err != nil {
+		return nil, err
+	}
+	return &DB{db},nil
+}
