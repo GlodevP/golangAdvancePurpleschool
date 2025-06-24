0edd323b63f846c2cf85932ae952118ebbb3e6b6
GlodevP <glodevp@gmail.com>
2025-06-24 09:40:12 +0300

    init payload

--- homeWorks/4-order-api/internal/order/payload.go
+++ homeWorks/4-order-api/internal/order/payload.go
@@ -1,1 +1,31 @@
 package order
+
+type GetOrderRequest struct{
+
+}
+type GetOrderResponce struct{
+
+}
+
+type GetOrdersRequest struct{
+
+}
+type GetOrdersResponce struct{
+
+}
+
+type AddOrderRequest struct{
+	
+}
+
+type AddOrderResponce struct{
+
+}
+
+type DeleteOrderRequest struct{
+	
+}
+
+type DeleteOrderResponce struct{
+
+}
