8ab171f3491024fbe0a45fcc7a045b8ba417a789
GlodevP <glodevp@gmail.com>
2025-06-24 09:40:01 +0300

    init config

--- homeWorks/4-order-api/cmd/main.go
+++ homeWorks/4-order-api/cmd/main.go
@@ -1,5 +1,8 @@
 package main
 
+import "4-order-api/config"
+
 func main(){
+	cfg := config.NewConfigs()
 	
 }
--- homeWorks/4-order-api/config/config.go
+++ homeWorks/4-order-api/config/config.go
@@ -1,1 +1,35 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct{
+	Webserver *WebserverConfig
+	DB *DBConfig
+}
+
+type WebserverConfig struct{
+	Addr string
+}
+type DBConfig struct{
+	DSN string
+}
+
+func NewConfigs() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Файл .env не загружен: %s\n", err)
+	}
+	return &Config{
+		Webserver: &WebserverConfig{
+			Addr: os.Getenv("WEB_SERVER_URL"),
+		},
+		DB: &DBConfig{
+			DSN: os.Getenv("DSN"),
+		},
+		}
+}
