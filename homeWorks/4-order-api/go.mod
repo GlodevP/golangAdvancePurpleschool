49f72786298b2afd35f768c97413f64cc6d7660c
GlodevP <glodevp@gmail.com>
2025-06-29 11:24:56 +0300

    add images

--- homeWorks/4-order-api/go.mod
+++ homeWorks/4-order-api/go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	golang.org/x/sync v0.15.0 // indirect
 	gorm.io/driver/postgres v1.6.0 // indirect
 	gorm.io/gorm v1.30.0 // indirect
--- homeWorks/4-order-api/internal/store/model.go
+++ homeWorks/4-order-api/internal/store/model.go
@@ -1,11 +1,15 @@
 package store
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"github.com/lib/pq"
+)
 
 type Product struct{
 	gorm.Model
 	Name string `json:"name" validate:"required" gorm:"not null"`
 	Description string `json:"description"`
+	Images pq.StringArray `gorm:"type:text[]" json:"images"`
 }
 
 func NewOrder(name string,description string)*Product{
